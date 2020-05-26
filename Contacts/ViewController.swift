65fd32ddbb755562b36a1d00a58562748cb05e02
Victor Pelivan <pelivan.v.1988@gmail.com>
2020-05-26 14:55:37 +0300

    Project Start

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Victor Pelivan on 26.05.2020.
+//  Copyright © 2020 Victor Pelivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
